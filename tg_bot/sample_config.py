4801210ff573ec99b1d67d83e052e4be1fad6f4e
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2021-03-10 16:46:14 +0530

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -18,13 +18,13 @@ class Config(object):
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
     MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
-    START_PHOTTO = None # Any img Url To shown In the Start Menu
     LOAD = []
     NO_LOAD = ['translation', 'rss']
     WEBHOOK = False
     URL = None
 
     # OPTIONAL
+    START_PHOTTO = None # Any img Url To shown In the Start Menu
     SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
     SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
     WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
